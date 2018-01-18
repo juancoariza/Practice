d2307d24c3f1f75c89834b0699c5007b6be76a5c
juancoariza <35510324+juancoariza@users.noreply.github.com>
2018-01-18 18:44:24 -0500

    Add files via upload

---
+++ MainCreation.java
@@ -0,0 +1,10 @@
+//Juan Ariza
+//Main Creation Integration Project
+public class MainCreation {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+	//When opening a project from Eclipse, ALWAYS import. 
+	//Never open a single file.
+}
