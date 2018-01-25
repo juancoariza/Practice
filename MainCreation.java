56d619e5565e8397f4057f2e254ac4a9f6b3cb2b
jaariza3783 <jaariza3783@L438R3S8.STUDENT.ad.fgcu.edu>
2018-01-25 18:41:58 -0500

    First Commit

--- MainCreation.java
+++ MainCreation.java
@@ -4,6 +4,7 @@ public class MainCreation {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("GitHub Syncing Test");
 	}
 	//When opening a project from Eclipse, ALWAYS import. 
 	//Never open a single file.
